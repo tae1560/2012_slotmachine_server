b68ca981f05aa4e253ef8bcbb7d2d0fe4d8d1d07
TAEHO LEE <tae1560@gmail.com>
2012-12-21 20:17:55 +0900

    Fixed some bugs

--- db/migrate/20121220032045_create_slot_logs.rb
+++ db/migrate/20121220032045_create_slot_logs.rb
@@ -2,7 +2,7 @@ class CreateSlotLogs < ActiveRecord::Migration
   def change
     create_table :slot_logs do |t|
       t.integer :device_id, :index => true
-      t.integer :db_id, :index => true, :unique => true
+      t.integer :db_id, :index => true
       t.integer :db_prize
       t.datetime :time
 
